f2f8703ffd8884cf624c87e1da963a5e63156926
ty0x2333 <luckytianyiyan@gmail.com>
2021-03-19 03:49:00 +0800

    Optimize Code

--- DimoLogo/Classes/Core/DimoLogoView.swift
+++ DimoLogo/Classes/Core/DimoLogoView.swift
@@ -27,7 +27,7 @@ public class DimoLogoView: UIView {
     private static let BaseReference: CGFloat = 60.0
     private static let WaterDropArcRadiusRatio: CGFloat = 1.0 / 12.0
     private static let WaterDropControlPointOffsetYRatio: CGFloat = 1.0 / 15.0
-    private static let WaterDroStretchLengthRatio: CGFloat = 1.0 / 15.0
+    private static let WaterDropStretchLengthRatio: CGFloat = 1.0 / 15.0
     private static let WaterDropElemMaxIntervalRatio: CGFloat = 7.0 / 30.0
     private static let LineMarginHorizontalRatio: CGFloat = 1.0 / 12.0
     private static let LineWidthRatio: CGFloat = 1.0 / 20.0
@@ -36,7 +36,7 @@ public class DimoLogoView: UIView {
     
     private var waterDropArcRadius: CGFloat = 5.0
     private var waterDropControlPointOffsetY: CGFloat = 4.0
-    private var waterDroStretchLength: CGFloat = 4.0
+    private var waterDropStretchLength: CGFloat = 4.0
     private var waterDropElemMaxInterval: CGFloat = 14.0
     private var lineMarginHorizontal: CGFloat = 5.0
     private var lineWidth: CGFloat = 3.0
@@ -48,15 +48,10 @@ public class DimoLogoView: UIView {
     private var popAnimationFinishedTime: CGFloat = 0
     private var repopAnimationFinishedTime: CGFloat = 0
     private var moveAnimationFinishedTime: CGFloat = 0
-    private var waterDropStretchBeginTime: CGFloat = 0
-    private var waterDropStretchFinishedTime: CGFloat = 0
-    private var fadeInBeginTime: CGFloat = 0
-    private var fadeInFinishedTime: CGFloat = 0
+    private var waterDropStretchTimeRange: ClosedRange<CGFloat> = 0...0
+    private var fadeInTimeRange: ClosedRange<CGFloat> = 0...0
     
-    private var lineStretchBeginTime: CGFloat = 0
-    private var lineStretchFinishedTime: CGFloat = 0
-    private var lineStretch2FinishedTime: CGFloat = 0
-    private var lineStretch3FinishedTime: CGFloat = 0
+    private var lineStretchTimes: [CGFloat] = [0, 0, 0, 0]
     
     private class DisplayLinkWrapper {
         private weak var target: AnyObject?
@@ -156,7 +151,7 @@ public class DimoLogoView: UIView {
         
         waterDropArcRadius = reference * DimoLogoView.WaterDropArcRadiusRatio
         
-        waterDroStretchLength = reference * DimoLogoView.WaterDroStretchLengthRatio
+        waterDropStretchLength = reference * DimoLogoView.WaterDropStretchLengthRatio
         
         lineMarginHorizontal = reference * DimoLogoView.LineMarginHorizontalRatio
         
@@ -177,15 +172,10 @@ public class DimoLogoView: UIView {
         popAnimationFinishedTime = animationDuration * 0.125
         repopAnimationFinishedTime = animationDuration * 0.15
         moveAnimationFinishedTime = animationDuration * 0.85
-        waterDropStretchBeginTime = animationDuration * 0.5
-        waterDropStretchFinishedTime = animationDuration * 0.875
-        fadeInBeginTime = animationDuration * 0.8
-        fadeInFinishedTime = animationDuration * 1
+        waterDropStretchTimeRange = (animationDuration * 0.5)...(animationDuration * 0.875)
+        fadeInTimeRange = (animationDuration * 0.8)...(animationDuration * 1)
         
-        lineStretchBeginTime = animationDuration * 0.5
-        lineStretchFinishedTime = animationDuration * 0.8
-        lineStretch2FinishedTime = animationDuration * 0.9
-        lineStretch3FinishedTime = animationDuration * 1
+        lineStretchTimes = [animationDuration * 0.5, animationDuration * 0.8, animationDuration * 0.9, animationDuration * 1]
     }
     
     override public func draw(_ rect: CGRect) {
@@ -204,9 +194,7 @@ public class DimoLogoView: UIView {
         let repopRange = popAnimationFinishedTime...repopAnimationFinishedTime
         let noMoveRange = 0.0...repopAnimationFinishedTime
         let moveRange = repopAnimationFinishedTime...moveAnimationFinishedTime
-        let noStretchRange = 0.0...waterDropStretchBeginTime
-        let stretchRange = waterDropStretchBeginTime...waterDropStretchFinishedTime
-        let alphaRange = fadeInBeginTime...fadeInFinishedTime
+        let noStretchRange = 0.0...waterDropStretchTimeRange.lowerBound
         var alpha: CGFloat = 1
         
         // move
@@ -231,14 +219,14 @@ public class DimoLogoView: UIView {
             context.drawPath(using: .fill)
         } else {
             
-            if time >= alphaRange.lowerBound {
-                let elapsed = time - alphaRange.lowerBound
-                alpha = 1 - elapsed / alphaRange.length
+            if time >= fadeInTimeRange.lowerBound {
+                let elapsed = time - fadeInTimeRange.lowerBound
+                alpha = 1 - elapsed / fadeInTimeRange.length
                 //                print("time: \(time) elapsed: \(elapsed) alpha: \(alpha)")
             }
             
-            let elapsed = time - stretchRange.lowerBound
-            waterDrop(process: min(1, elapsed / stretchRange.length), arcCenter: arcCenter).fill(with: .normal, alpha: alpha)
+            let elapsed = time - waterDropStretchTimeRange.lowerBound
+            waterDrop(process: min(1, elapsed / waterDropStretchTimeRange.length), arcCenter: arcCenter).fill(with: .normal, alpha: alpha)
         }
     }
     
@@ -252,9 +240,9 @@ public class DimoLogoView: UIView {
         let maxOffsets: [CGFloat] = lineStretchLengths
         var centerPoint = CGPoint(x: centerX, y: centerY)
         
-        let stretchRanges: [ClosedRange<CGFloat>] = [lineStretchBeginTime...lineStretchFinishedTime,
-                                                     lineStretchFinishedTime...lineStretch2FinishedTime,
-                                                     lineStretch2FinishedTime...lineStretch3FinishedTime]
+        let stretchRanges: [ClosedRange<CGFloat>] = [lineStretchTimes[0]...lineStretchTimes[1],
+                                                     lineStretchTimes[1]...lineStretchTimes[2],
+                                                     lineStretchTimes[2]...lineStretchTimes[3]]
         
         for i in 0..<3 {
             let range = stretchRanges[i]
@@ -294,7 +282,7 @@ public class DimoLogoView: UIView {
     }
     
     private func waterDrop(process: CGFloat, arcCenter: CGPoint) -> UIBezierPath {
-        let waterDropTop = arcCenter.offset(offsetY: -waterDropArcRadius - waterDroStretchLength * process)
+        let waterDropTop = arcCenter.offset(offsetY: -waterDropArcRadius - waterDropStretchLength * process)
         
         let waterDrop = UIBezierPath()
         waterDrop.move(to: waterDropTop)
@@ -309,6 +297,7 @@ public class DimoLogoView: UIView {
     @objc private func fire() {
         time += 0.05
         setNeedsDisplay()
+        print("fire")
     }
     
 }
